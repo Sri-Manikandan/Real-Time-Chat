1e199cf5d44eb767cfa48ffa62906fe51457a711
Sri Manikandan R <srimanikandandev@gmail.com>
2024-01-07 09:11:30 +0530

    initializing the project

---
+++ Server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+require('dotenv').config();
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
+
+app.listen(port,(req,res)=>{
+    console.log(`Server running at port ${port}`);
+});
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(()=> console.log('MongoDB connected'))
+.catch(err => console.log("MongoDB connection error: ", err.message));
